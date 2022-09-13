640fd56eebe6ab29858e8895444be0abe1fcdd13
ruslanraupoff <ruslanraupoff@gmail.com>
2022-09-13 12:07:09 +0900

    coap added

--- samples/server/main.js
+++ samples/server/main.js
@@ -1,6 +1,7 @@
 const WebSocketServer = require('ws').Server
 const bodyParser = require('body-parser')
 const express = require('express')
+const coap = require('coap')
 const app = express()
 
 const port = 3000
@@ -25,7 +26,7 @@ app.all('/*', function (req, res) {
 	res.end()
 })
 
-// start the server:
+// start the http server:
 const server = app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
@@ -41,4 +42,38 @@ wss.on('connection', function connection(ws) {
 		// echo it back
 		ws.send(message)
 	})
+})
+
+// create a CoAP server
+const CoapServer = coap.createServer()
+
+CoapServer.on('request', (req, res) => {
+	console.log('Got a ' + req.method + ' request')
+	// the parameters of a GET request are passed in
+	// request.body. Pass that to formatResponse()
+	// for formatting:
+	console.log(req.headers)
+	if (req.method == 'GET') {
+		console.log(req.query)
+	} else {
+		console.log(req.body)
+	}
+
+	// send the response:
+	res.send('OK')
+	res.end()
+})
+
+// the default CoAP port is 5683
+CoapServer.listen(() => {
+  const req = coap.request(`coap://0.0.0.0:${port}/arduino`)
+
+  req.on('response', (res) => {
+    res.pipe(process.stdout)
+    res.on('end', () => {
+      process.exit(0)
+    })
+  })
+
+  req.end()
 })
